b25d0cc7e2195597ba50b96ef0bdfdaca4b0e370
sunilcverma <sunilcverma@gmail.com>
2013-07-09 12:30:10 +0000

    Updated copyright information.

--- FoxSol/includes/footer.php
+++ FoxSol/includes/footer.php
@@ -6,7 +6,7 @@ echo '<div id="footer-content">
 		
 			
 				<p>
-		<center>&copy; copyright 2012 <strong>Black Tornado Team</strong><br /> 
+		<center>&copy; copyright 2013 <strong>Forest Corporation</strong><br /> 
 		Powered by: <a href="#"><strong>HPIE</strong></a>';		// Design by: <a href="#"><strong>Rajesh & Bhanu</strong></a></center>
 		
 	echo '	</p>
